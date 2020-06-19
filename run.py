06afacbd8aa7ce2a1cbd3287d3859526a2830214
Skye Long <skye.m.long@gmail.com>
2020-06-19 17:23:04 -0400

    cleaning

---
+++ run.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 15 16:20:01 2020
+
+@author: macbook
+"""
+#!/usr/bin/env python
+from flaskexample import app
+app.run(debug = True)
